f4d1a4fcd93157932b0c339b998a7235cd6167eb
Nicolas Brugneaux <nicolas@passbase.com>
2020-10-15 20:44:48 +0200

    release: v1.0.2

--- src/main/java/com/passbase/ApiClient.java
+++ src/main/java/com/passbase/ApiClient.java
@@ -84,7 +84,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("passbase/java@1.0.1");
+        setUserAgent("passbase/java@1.0.2");
 
         // Setup authentications (key: authentication name, value: authentication).
         authentications = new HashMap<String, Authentication>();
